f5b563b51668de532db9b2131032f2c11a6a8d3e
D4-CTM <josuedelcid325@gmail.com>
2026-05-13 09:34:32 -0600

    Added CaiRange table. - Changed Cai Model - Created CaiRange Model - Added foreignKeys

--- Service/models/index.js
+++ Service/models/index.js
@@ -19,6 +19,10 @@ if (config.use_env_variable) {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+sequelize.authenticate()
+    .then(() => console.log('Success'))
+    .catch((reason) => console.log(reason))
+
 sequelize.createSchema('cd')
     .catch((reason) => console.log(`Failed to create schema cause: ${reason}`));
 
