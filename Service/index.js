605bf612b5eb4c06514e54fe8ba80e9c3bf2011e
RoyGeova07 <royumana3@gmail.com>
2026-05-16 17:55:35 -0600

    resolviendo conflictos

--- Service/index.js
+++ Service/index.js
@@ -21,7 +21,7 @@ app.use('/api', companyRoutes)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpecs))
 app.listen(port, ()=>
 {
 
