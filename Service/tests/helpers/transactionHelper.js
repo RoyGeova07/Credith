10b9b89f3b859cf41a7fe2ba634db0cda64b575e
D4-CTM <josuedelcid325@gmail.com>
2026-05-23 16:18:32 -0600

    Feature: database transaction spy. - Added helper to avoid commiting db changes in tests.

---
+++ Service/tests/helpers/transactionHelper.js
@@ -0,0 +1,26 @@
+const db = require('../../models');
+
+let managedTransaction;
+
+async function setupTestTransaction() {
+  managedTransaction = await db.sequelize.transaction();
+
+  const original = db.sequelize.transaction.bind(db.sequelize);
+  jest.spyOn(db.sequelize, 'transaction').mockImplementation(async (options, callback) => {
+    if (typeof options === 'function') {
+      callback = options;
+      options = {};
+    }
+    return original({ ...options, transaction: managedTransaction }, callback);
+  });
+}
+
+async function teardownTestTransaction() {
+  jest.restoreAllMocks();
+  if (managedTransaction) {
+    await managedTransaction.rollback();
+    managedTransaction = null;
+  }
+}
+
+module.exports = { setupTestTransaction, teardownTestTransaction };
