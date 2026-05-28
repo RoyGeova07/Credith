f05e4c761a1195b98136a46c8221ca56c2b930a5
D4-CTM <josuedelcid325@gmail.com>
2026-05-28 15:20:20 -0600

    Removed unnecessary css

--- Website/src/App.jsx
+++ Website/src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/home'
 import RegisterPage from './pages/RegisterPage'
-
-import './App.css'
-
+import Home from './pages/home'
 
 export default function App() {
     return (
